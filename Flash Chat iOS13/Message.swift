0d220ff0954e80307fc35b16b8e61d7833c0c084
David <davidtucu@hotmail.com>
2021-06-09 06:55:56 -0500

    Initial Commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by david on 04/06/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email of sender user
+    let body: String
+}
